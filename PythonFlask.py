d0ed15c0e461993d2def63613e8e4e0f3054011c
赵磊 <pilot_lei@foxmail.com>
2016-05-15 13:59:05 +0800

    加入404定向，图片等

--- PythonFlask.py
+++ PythonFlask.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+
 @app.route('/')
 def hello_world():
-    return 'Index Page'
+    return url_for('show_user_profile', username='Jolly')
 
 
 @app.route('/hello')
