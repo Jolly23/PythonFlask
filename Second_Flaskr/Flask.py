85ff2b37a1cb06dd2c8c3365e3f4c65282e0276c
赵磊 <pilot_lei@foxmail.com>
2016-05-16 08:10:31 +0800

    初步实现Flaskr

--- Second_Flaskr/Flask.py
+++ Second_Flaskr/Flask.py
@@ -15,9 +15,13 @@ PASSWORD = 'admin'
 
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-'''
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-'''
+# app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+if __name__ == '__main__':
+    app.run()
