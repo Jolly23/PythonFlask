7ea823fec4dc9faf1b07dc82ddd24fbbae520f45
赵磊 <pilot_lei@foxmail.com>
2016-05-16 20:04:12 +0800

    临时间加入config

---
+++ Second_Flaskr/config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+MAIL_HOST = 'applesmtp.163.com'
+FROM_ADDR = 'server-error@example.com'
+ADMINS = ['pilot_lei@foxmail.com']  # To address
+SUBJECT = 'YourApplication Failed'
+CREDENTIALS = ('lionel_lei@163.com', 'Arsenal2')
