ef814348d87d094732a6d57aef7a57f9d3414e62
赵磊 <pilot_lei@foxmail.com>
2016-05-16 08:29:51 +0800

    Flaskr创建数据库

--- Second_Flaskr/Flaskr.py
+++ Second_Flaskr/Flaskr.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
 import sqlite3
+from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from flask.ext.sqlalchemy import SQLAlchemy
 
 # configuration
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DATABASE = 'sqlite:///' + os.path.join(basedir, 'app.db')
+DATABASE = os.path.join(basedir, 'flaskr.db')
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
@@ -15,13 +16,22 @@ PASSWORD = 'admin'
 
 
 app = Flask(__name__)
-# app.config.from_object(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config.from_object(__name__)
+# app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
 
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
 if __name__ == '__main__':
     app.run()
+
+
