3e6fc97b49d8d3b72ce03dda659564e9e66712be
Jolly23 <pilot_lei@foxmail.com>
2016-06-15 12:48:47 +0800

    fix

--- Second_Flaskr/Flaskr.py
+++ Second_Flaskr/Flaskr.py
@@ -123,10 +123,8 @@ def upload_file():
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            file.save(os.path.join('/root/PythonFlask/Second_Flaskr/tmp', filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
     return '''
     <!doctype html>
     <title>Upload new File</title>
